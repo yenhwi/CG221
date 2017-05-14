7606a668abbc68111186111f495cc8aea6e26cdf
yenhwi <yenhwi@gmail.com>
2017-05-14 18:20:22 -0400

    Add files via upload

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,110 +1,111 @@
-var dot;
-var shape;
-var spawnPoint;
-var bgColor;
+var t = 1;
+var r = 30;
+var g = 100;
+var b = 200;
+var song1;
+var song2;
+var song3;
+var song4;
+var img;
+
+function preload() {
+  song1 = loadSound('Itantme.mp3');
+  song2 = loadSound('24.mp3');
+  song3 = loadSound('Marshmello.mp3');
+  song4 = loadSound('Paris.mp3');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  spawnPoint = windowHeight;
-  bgColor = color(21, 85, 209);
-  frameRate(48);
-  dot = new Dot();
-  shape = new Polygon()
+  createCanvas(1450, 800, WEBGL);
+  noStroke();
+  img = ("http://files.effectsdatabase.com/brands/thumbs/colorsound_000.jpg")
+  img.position(500, 500)
+  img.mousePressed(uniHide);
+  textSize(40);
+  text('Move the mouse to the background color that you like, and the music will change', 20, 50);
 }
 
 function draw() {
-  background(bgColor);
 
-  dot.update();
-  shape.draw();
-  drawCover();
-  watchDot();
+strokeWeight(2);
+fill(128 + sin(frameCount*0.1) * 128);
+if (mouseIsPressed) {
+  stroke(255);
 }
-
-var watchDot = function() {
-  if (dot.getY() > spawnPoint) {
-    dot = new Dot();
-    shape = new Polygon();
-  }
-}
-
-var drawCover = function() {
+else {
   noStroke();
-  fill(bgColor);
-  rect(0, round(windowHeight/1.5), windowWidth, windowHeight/2);
-  fill(255);
-  rect(round(windowWidth/5) + round(windowWidth/5), round(windowHeight/1.5), round(windowWidth/5), 1);
+}
+textSize(12 + (mouseX / width)*72);
+text("Attention, please.", 50, 200);
 }
 
-var Dot = function() {
-  var velConst      = windowHeight / 40;
-  var gravConst     = velConst / 60;
-  var location      = createVector(windowWidth / 2, spawnPoint);
-  var gravity       = createVector(0, gravConst);
-  var velocity      = createVector(0, 0);
-  var acceleration  = createVector(0, -velConst);
-  var topSpeed      = velConst;
-
-  var update = function() {
-    velocity.add(acceleration);
-    velocity.add(gravity);
-    velocity.limit(topSpeed);
-    location.add(velocity);
-    acceleration.mult(0);
+  if(mouseIsPressed){
+    background('#ff9999');
+  } else {
+    background(mouseX, 60, mouseY);
   }
 
-  var draw = function() {
-    noStroke();
-    fill(255);
-    ellipse(location.x, location.y, 3, 3);
-    update();
-  }
+  //basicMaterial(255, 0, 0);
+  ambientLight(255, 0, 0);
 
-  var getY = function() {
-    return location.y;
-  }
+  directionalLight(255, 100, 0, 0.1, -1, 0, sin(t / 2));
 
-  return {
-    'draw': draw,
-    'update': update,
-    'getY': getY
-  }
+  directionalLight(0, 0, 200, 0.1, 1, 0, sin(t));
+
+  //  	directionalLight(0, 0, 255, 0.1,  0.5, 1, sin(t*10));
+
+  rotateY(frameCount / 100);
+  rotateX(map(sin(t), -1, 1, -PI, PI));
+  sphere(200, 50);
+
+  t += 0.025;
+
 }
 
-var Polygon = function() {
-  var sides     = Math.floor(random(3, 7));
-  var cx        = windowWidth/2;
-  var cy;
-  var angle     = random(0, 360);
-  var radius    = windowWidth / 12;
-  var angSpeed  = random(-10, 10);
-  var color     = floor(random(0, 2)) % 2 ? 0 : 255;
+function uniHide{
+  img.hide();
+}
 
-  var updatePoints = function() {
-    cy = dot.getY();
-    points = [];
-    for (var i = 0; i < sides; i++) {
-      var a = angle + (i / sides) * 360;
-      var x = cx + (cos(radians(a)) * radius);
-      var y = cy + (sin(radians(a)) * radius);
-      points.push([x, y]);
+function unShow(){
+  img.show();
+}
+
+function keyPressed(){
+  uniShow();
+}
+
+function mouseMoved() {
+  if ( mouseX > 600 && mouseY > 400) {
+    if (song1.isPlaying() == false ){ //if this song is not playing
+      song1.play(); //play this song
     }
-    angle = angle + angSpeed;
-  }
-
-  var draw = function() {
-    updatePoints();
-
-    fill(color);
-    noStroke();
-    beginShape();
-    for (var i = 0; i < sides; i++) {
-      vertex(points[i][0], points[i][1]);
+    //stop all other songs from playing
+    song2.stop();
+    song3.stop();
+    song4.stop();
+  } else if ( mouseX > 600 && mouseY < 400) {
+    if (song2.isPlaying() == false ){ //if this song is not playing
+      song2.play();
     }
-    endShape(CLOSE);
-  }
-
-  return {
-    'draw': draw
+    //stop all other songs from playing
+    song1.stop();
+    song3.stop();
+    song4.stop();
+  } else if ( mouseX < 600 && mouseY < 400) {
+    if (song3.isPlaying() == false ){ //if this song is not playing
+      song3.play();
+    }
+    //stop all other songs from playing
+    song1.stop();
+    song2.stop();
+    song4.stop();
+  } else {
+    if (song4.isPlaying() == false ){ //if this song is not playing
+      song4.play();
+    }
+    //stop all other songs from playing
+    song1.stop();
+    song2.stop();
+    song3.stop();
   }
 }
